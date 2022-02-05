8c79c95002bde8aa973e5b39ff4e152005849705
K_Kwon <ts04031@gmail.com>
2022-02-06 06:27:58 +0900

    이분 탐색
    파라메트릭 서치를 통해서 최댓값을 찾는 문제

---
+++ 이분 탐색/1654_랜선 자르기.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	int k, n;
+	cin >> k >> n;
+	
+	vector<long long> v(n);
+	long long max = 0;
+	for (int i = 0; i < k; i++) {
+		cin >> v[i];
+		if (max < v[i])
+			max = v[i];
+	}
+
+	long long start = 1;
+	long long high = max;
+	int result = 0;
+	while (start <= high) {
+		long long mid = (start + high) / 2; // 중간값을 랜선값으로 먼저 확인
+		long long sum = 0;
+		for (int i = 0; i < k; i++) {
+			sum += v[i] / mid;
+		}
+		if (sum < n) {
+			high = mid - 1;
+		}
+		else { // sum >= n
+			if (result < mid)
+				result = mid;
+			start = mid + 1;
+		}
+	}
+	
+	cout << result << '\n';
+
+	return 0;
+}
